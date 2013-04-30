5202a1894c0196a3d5f06652a783ecf2058e02f9
Thomas Garver <tgarv@mit.edu>
2013-04-29 21:34:48 -0400

    Removed main() in s3_utils

--- s3_utils.go
+++ s3_utils.go
@@ -94,19 +94,3 @@ func SplitFileIntoChunks(filepath string, bucket s3.Bucket, directory string, ch
 
 	return true
 }
-
-func main() {
-	bucket := GetBucket()
-	fmt.Printf("Bucket: %s\n", bucket)
-
-	// SplitFileIntoChunks("../../final_project/texts/output.txt", bucket, "small_test3", 100000)
-
-	// jobs := []WorkerJob{}
-
-	keys := FilterKeysByPrefix(bucket, "small_test3/")	// Print out all the keys/chunks in the small_test/ directory
-	for _, key := range keys {
-		fmt.Println(key)
-		// jobs = append(jobs, WorkerJob{key, "", false, map[string]string{}})
-	}
-
-}
