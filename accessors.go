0f89002ff6fd74e7ee557bd869affec2efbc22b4
Dalton Hubble <dghubble@gmail.com>
2013-05-10 18:07:29 -0400

    Moved Inputer and S3Inputter back to accessors.go temporarily.

--- accessors.go
+++ accessors.go
@@ -2,15 +2,42 @@ package gomapreduce
 
 
 import (
-	"fmt"
-	"strings"
 	"strconv"
+	"strings"
+	"fmt"
 )
 
+type Inputer interface {
+	ListKeys() []string 			// A method to list all of the keys (i.e. all of the map jobs)
+	GetValue(key string) string 	// A method to get the value for one of the keys
+}
 
+type S3Inputer struct { 	// Satisfies the InputAccessor interface
+	Folder string
+}
 
+// S3 Inputer Constructor
+func MakeS3Inputer (inputFolder string) S3Inputer {	
+	if !strings.HasSuffix(inputFolder, "/") { 	// Make sure that the folder has a trailing slash
+		inputFolder += "/"
+	}
+	fmt.Printf("Folder: %s\n", inputFolder)
+	accessor := S3Inputer{Folder: inputFolder}
 
+	return accessor
+}
 
+func (self S3Inputer) ListKeys() []string {
+	bucket := GetBucket()
+	keys := FilterKeysByPrefix(bucket, self.Folder)
+	return keys
+}
+
+func (self S3Inputer) GetValue(key string) string {
+	bucket := GetBucket()
+	value := GetObject(bucket, key)
+	return string(value)
+}
 
 
 
